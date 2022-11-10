d7c58dfe2af6b401817873582e33870f258cb3b1
MatijaMikulic <matijamikul@gmail.com>
2022-11-10 17:51:24 +0100

    task2

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        public string[] Get()
+        {
+            return new string[]
+            {
+        "Hello",
+        "World"
+            };
+        }
+    }
+}
